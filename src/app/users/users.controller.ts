ec0ffd0a33b7d2f412f474f6bdbd568126c51c26
RomanPantya <romanpantya@gmail.com>
2023-01-01 03:22:09 +0200

    add UpdateUser Dto & moks crud

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get(':id')
+  getOne(@Param('id') id: string): string {
+    return this.usersService.getOne(id);
+  }
+
   @Get()
-  getHello() {
-    return this.usersService.getHello();
+  getAll(): string {
+    return this.usersService.getAll();
+  }
+
+  @Post()
+  create(@Body() createUser: CreateUserDto): object {
+    return this.usersService.create(createUser);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return this.usersService.remove(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUser: UpdateUserDto): object {
+    return this.usersService.update(id, updateUser);
   }
 }
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello Romario!';
+  getOne(id: string): string {
+    return 'user ' + id;
+  }
+
+  getAll(): string {
+    return 'Thats all users';
+  }
+
+  create(createUser: CreateUserDto): object {
+    return { message: `This user was create`, data: createUser };
+  }
+
+  remove(id: string) {
+    return 'This user was delete: ' + id;
+  }
+
+  update(id: string, updateUser: UpdateUserDto): object {
+    return { message: `user with id: ${id} was update`, data: updateUser };
   }
 }
