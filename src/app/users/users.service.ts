b1457f5bfb73949b870e591da0af46b0d8d1d9e8
RomanPantya <romanpantya@gmail.com>
2023-01-02 01:36:11 +0200

    change users.service, add repository UserEntity

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  getOne(id: string): string {
-    return 'user ' + id;
-  }
+    constructor(
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>
+    ) {}
+    getOne(id: string): string {
+        return 'user ' + id;
+    }
 
-  getAll(): string {
-    return 'Thats all users';
-  }
+    getAll(): string {
+        return 'Thats all users';
+    }
 
-  create(createUser: CreateUserDto): object {
-    return { message: `This user was create`, data: createUser };
-  }
+    create(createUser: CreateUserDto): object {
+        return { message: `This user was create`, data: createUser };
+    }
 
-  remove(id: string) {
-    return 'This user was delete: ' + id;
-  }
+    remove(id: string) {
+        return 'This user was delete: ' + id;
+    }
 
-  update(id: string, updateUser: UpdateUserDto): object {
-    return { message: `user with id: ${id} was update`, data: updateUser };
-  }
+    update(id: string, updateUser: UpdateUserDto): object {
+        return { message: `user with id: ${id} was update`, data: updateUser };
+    }
 }
