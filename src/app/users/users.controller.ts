786d9ef0b138f06664fe9acfcf1098143f0a2c6b
RomanPantya <romanpantya@gmail.com>
2023-01-01 23:01:51 +0200

    refactor

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -13,30 +13,30 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return this.usersService.getOne(id);
-  }
+    getOne(@Param('id') id: string): string {
+        return this.usersService.getOne(id);
+    }
 
   @Get()
   getAll(): string {
-    return this.usersService.getAll();
+      return this.usersService.getAll();
   }
 
   @Post()
   create(@Body() createUser: CreateUserDto): object {
-    return this.usersService.create(createUser);
+      return this.usersService.create(createUser);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string): string {
-    return this.usersService.remove(id);
+      return this.usersService.remove(id);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateUser: UpdateUserDto): object {
-    return this.usersService.update(id, updateUser);
+      return this.usersService.update(id, updateUser);
   }
 }
