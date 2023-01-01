c7d92584d05343bc4cab3acb22ad4d8377064b1b
RomanPantya <romanpantya@gmail.com>
2023-01-01 04:07:37 +0200

    npm install dotenv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    
+  })],
 })
 export class AppModule {}
